e1622aa80bc728d0f8b0e27eef34514935dd9403
Daniele Campari <danielecampari@gmail.com>
2022-08-31 00:08:27 +0200

    function displayLibrary: basic logic

--- main.js
+++ main.js
@@ -30,8 +30,55 @@ function addBookToLibrary (book) {
     myLibrary.push(book);
 }
 
-/* TEST
-const b = new Book('The Hobbit', 'JRRR Tolkien', 345, true);
+function displayLibrary (library) {
+    const section = document.querySelector('#library');
+
+    library.forEach(book => {
+
+        const card = document.createElement('div');
+        section.appendChild(card);
+        const title = document.createElement('h2');
+        title.textContent = book.title;
+        card.appendChild(title);
+        const author = document.createElement('p');
+        author.textContent = book.author;
+        card.appendChild(author);
+        const pages = document.createElement('span');
+        pages.textContent = `N. of pages: ${book.pages}`;
+        card.appendChild(pages);
+
+        const read = document.createElement('span');
+        read.textContent = book.alreadyRead ? 'Book already read' : 'Not read yet';
+        card.appendChild(read);  
+    });
+}
+
+
+/* TEST */
+// Create instances and add those to array
+const hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 305, true);
+addBookToLibrary(hobbit);
+
+const silmarillon = new Book ('The Silmarillion', 'J.R.R. Tolkien', 405, true);
+addBookToLibrary(silmarillon);
+
+const lord = new Book ('The Lord of the Rings', 'J.R.R. Tolkien', 1137, true);
+addBookToLibrary(lord);
+
+const fall = new Book ('The Fall of Nùmenor', 'J.R.R. Tolkien', 352, false);
+addBookToLibrary(fall);
+
+const unfinished = new Book ('Unfinished Tales', 'J.R.R. Tolkien', 624, true);
+addBookToLibrary(unfinished);
+
+const gondolin = new Book ('The Fall of Gondolin', 'J.R.R. Tolkien', 304,
+false);
+addBookToLibrary(gondolin);
+
+console.log(myLibrary)
+displayLibrary(myLibrary);
+
+/*
 console.log(b);
 console.log(b.info());
 
